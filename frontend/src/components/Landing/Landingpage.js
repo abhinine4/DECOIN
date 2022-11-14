42b35f6ef0a957cc120c4f0aef2f8d4a4bd1bd0b
ashish077 <ash.coolest7@gmail.com>
2022-11-13 19:48:50 -0500

    structure

---
+++ frontend/src/components/Landing/Landingpage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Landingpage = () => {
+  return (
+    <div>
+         
+      <>hero</>
+    <>howitworks</>
+    <>Footer</>
+    </div>
+  )
+}
+
+export default Landingpage
